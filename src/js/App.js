8cba40144455a65fe7b8280636bf70175a1d7903
Hell1on <artem.bondarchuk1233@gmail.com>
2024-05-19 16:17:18 +0300

    added more functionality

--- src/js/App.js
+++ src/js/App.js
@@ -1,9 +1,15 @@
 'use strict';
 
+const dealerPoints = document.querySelector("#dealer-points");
+const playerPoints = document.querySelector("#player-points");
+const standButton = document.querySelector("#stand");
+const hitButton = document.querySelector("#hit");
+
 const SUITS = ['h', 'd', 'c', 's'];
 const VALUES = ['2', '3', '4', '5', '6', '7',
   '8', '9', '10', 'j', 'q', 'k', 'a'];
-
+const DealerHand = [];
+const PlayerHand = [];
 
 const initializeDeck = () => {
   const deck = SUITS.reduce((deckAcc, suit) =>{
@@ -30,53 +36,69 @@ const DECK = initializeDeck();
 
 const dealCard = (hand, visibility = 1) => {
   const card = DECK.pop();
-  let cardImageName = `${card.value}_${card.suit}`;
+  if (hand === 'dealer-hands'){
+    DealerHand.push(card)
+  } else PlayerHand.push(card)
+  const cardImageName = `${card.value}_${card.suit}`;
   let invisible = '';
   if (!visibility) {
     invisible = 'invisible';
   }
 
+  calculateHands();
+
   const ol = document.getElementsByClassName(hand)[0];
-  ol.innerHTML += `<li class="playing-card-item ${invisible}"> <img src="cards/${cardImageName}.png" alt="${cardImageName}"></li>`
-  return card;
+  ol.innerHTML += `<li id="card${visibility + 1}" class="playing-card-item ${invisible}"> <img src="cards/${cardImageName}.png" alt="${cardImageName}"></li>`
+};
+
+const calculateHands = (noStand=1) => {
+  const calculatingPoints = (hand) => {
+    let sum = 0;
+
+    for (const card of hand) {
+      if (card.value === 'a') {
+        if (sum + 11 > 21) {
+          sum++;
+        } else sum += 11;
+      } else if (['j', 'q', 'k'].includes(card.value)) {
+        sum += 10;
+      } else {
+        sum += parseInt(card.value);
+      }
+      if (hand === DealerHand && noStand) {
+        return sum;
+      }
+    }
+    return sum
+  };
+
+  const sumPointsDealer = calculatingPoints(DealerHand);
+  const sumPointsPlayer = calculatingPoints(PlayerHand);
+  dealerPoints.innerHTML = `Points: ${sumPointsDealer}`;
+  playerPoints.innerHTML = `Points: ${sumPointsPlayer}`;
 };
 
 const dealCards = () => {
-  const dealerHand = [];
-  const playerHand = [];
-
   for (let i = 0; i < 2; i++) {
     if (i === 1){
-      dealerHand.push(dealCard('dealer-hands', 0))
+      dealCard('dealer-hands', 0);
       continue;
     }
-    dealerHand.push(dealCard('dealer-hands'));
+    dealCard('dealer-hands');
   }
   for (let i = 0; i < 2; i++) {
-    playerHand.push(dealCard('player-hands'));
+    dealCard('player-hands');
   }
 
-  return [dealerHand, playerHand]
+  calculateHands()
 }
 
-const [dealerHand, playerHand] = [...dealCards()]
 
-console.log(dealerHand, playerHand)
+dealCards()
 
-const calculateHand = (hand) => {
-  let sum = 0;
-
-  for (const card of hand) {
-    if (card.value === 'A') {
-      if (sum + 11 > 21) {
-        sum++;
-      } else sum += 11;
-    } else if (['j', 'q', 'k'].includes(card.value)) {
-      sum += 10;
-    } else {
-      sum += parseInt(card.value);
-    }
-  }
-  return sum;
-};
+standButton.addEventListener("click", () => {
+  const hideCard = document.getElementsByClassName("invisible")[0];
+  hideCard.classList.remove("invisible")
+  calculateHands(0)
+}, )
 
